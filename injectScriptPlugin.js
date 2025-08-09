dcbde4c6795026063d08333e9b50f24727f84036
Blockchain Oracle <abubakrjimoh16488@gmail.com>
2025-08-09 15:13:17 +0100

    feat: add Google Ads conversion tracking
    Integrated Google Ads conversion tracking (ID: AW-10841395521) to measure website activity and track conversions. The tracking code is added through the existing injectScriptPlugin.js to ensure it loads on all pages.

--- injectScriptPlugin.js
+++ injectScriptPlugin.js
@@ -12,6 +12,23 @@ module.exports = function(context, options) {
               'data-website-id': '09a60445-7371-492c-9d75-c17be79fa569',
             },
           },
+          // Google Ads tag (gtag.js)
+          {
+            tagName: 'script',
+            attributes: {
+              async: true,
+              src: 'https://www.googletagmanager.com/gtag/js?id=AW-10841395521',
+            },
+          },
+          {
+            tagName: 'script',
+            innerHTML: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-10841395521');
+            `,
+          },
         ],
       };
     },
